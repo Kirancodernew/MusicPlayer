2e717fb0ab8151b02adddb5ac6e65ade931d0070
Rathod Saikiran <134682153+Kirancodernew@users.noreply.github.com>
2024-12-07 18:25:43 +0530

    Update Login.jsx

--- user/src/pages/Login.jsx
+++ user/src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
     try {
       // Send login request to the server
       const { data } = await axios.post(
-        "http://localhost:8080/api/v1/login",
+        "https://musicplayer-backend-nub4.onrender.com",
         formData
       );
 
